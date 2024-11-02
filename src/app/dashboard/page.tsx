de417b1cd6d50aeadafaab310a1e3964918bc36f
jorniks <dictforum@gmail.com>
2024-11-02 16:59:50 +0100

    Update: modified dashboard page and finished family page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,16 +18,17 @@ import useContractRead from '@/hooks/read-hooks/useContractRead';
 import { FamilyMemberType, FamilyType } from "@/types";
 import { useWeb3React } from "@web3-react/core";
 import { formatNumberScale } from "@/functions/format";
+import { Switch } from "@/components/switch";
 
 export default function Home() {
   const { account } = useWeb3React()
   const { addFamilyMember } = useContractWrite();
   const { getUserFamilies } = useContractRead()
   const [listOfFamilies, setListOfFamilies] = useState<FamilyType[]>([])
-  const [familyMembers, setFamilyMembers] = useState<FamilyMemberType[]>([])
   const [openNewMember, setOpenNewMember] = useState<boolean>(false)
   const [memberName, setMemberName] = useState<string>('')
   const [memberAddress, setAddressName] = useState<string>('')
+  const [personIsParent, setPersonIsParent] = useState<boolean>(false)
   const activeFamily = 0;
 
   const loadData = useCallback(
@@ -44,7 +45,7 @@ export default function Home() {
 
 
   return (
-    <main className="h-ful">
+    <main className="h-full">
       <section className="bg-white border-t">
         <div className="container flex flex-col md:flex-row md:items-center md:justify-between py-8 gap-8">
           <div className="space-x-4 flex items-center">
@@ -54,16 +55,19 @@ export default function Home() {
 
             <div className="text-xs">
               <h6 className="text-lg font-semibold">{ listOfFamilies[activeFamily]?.name }&apos;s Family</h6>
-              <span className="">{ formatNumberScale(listOfFamilies[activeFamily]?.memberCount || 0) } Family members</span>
+              <span className="">{ formatNumberScale(listOfFamilies[activeFamily]?.members?.length || 0) } Family members</span>
             </div>
           </div>
           
           <div className="flex flex-col md:flex-row md:items-center gap-x-10 gap-y-4 md:gap-y-0">
             <div className="flex -space-x-2 overflow-hidden text-lg">
-              <div className="flex items-center justify-center font-semibold h-10 w-10 bg-green-400 rounded-full ring-2 ring-white">sd</div>
-              <div className="flex items-center justify-center font-semibold h-10 w-10 bg-green-400 rounded-full ring-2 ring-white">sd</div>
-              <div className="flex items-center justify-center font-semibold h-10 w-10 bg-green-400 rounded-full ring-2 ring-white">sd</div>
-              <div className="flex items-center justify-center font-medium text-xs h-10 w-10 bg-green-400 rounded-full ring-2 ring-white">+3</div>
+              {listOfFamilies[activeFamily]?.members?.slice(0, 3).map((member, index) => (
+                <div key={index} className="flex items-center justify-center font-semibold h-10 w-10 bg-green-400 rounded-full ring-2 ring-white">{member?.name[0]}</div>
+              ))}
+
+              {listOfFamilies[activeFamily]?.members?.length > 3 &&
+                <div className="flex items-center justify-center font-medium text-xs h-10 w-10 bg-green-400 rounded-full ring-2 ring-white">+{listOfFamilies[activeFamily]?.members?.length - 3}</div>
+              }
             </div>
 
             <Dialog open={openNewMember} onOpenChange={setOpenNewMember}>
@@ -80,7 +84,7 @@ export default function Home() {
                   </DialogDescription>
                 </DialogHeader>
 
-                <section className="space-y-6 mt-6 w-full pb-8">
+                <section className="space-y-6 mt-6">
                   <div className="space-y-1">
                     <label htmlFor="memberName" className="block text-sm/6 font-medium">Name of Family Member</label>
                     <input id="memberName" type="text" className="text-box" value={memberName} onChange={(e) => setMemberName(e.target.value)} />
@@ -91,8 +95,18 @@ export default function Home() {
                     <input id="memberAddress" type="text" className="text-box" value={memberAddress} onChange={(e) => setAddressName(e.target.value)} />
                   </div>
 
+                  <div className="space-y-1">
+                    <label htmlFor="memberAddress" className="block text-sm/6 font-medium">Is this family member a parent?</label>
+                    <div className="gap-x-3 flex items-center">
+                      <label className="block text-sm font-medium">No</label>
+                      <Switch checked={personIsParent} onClick={() => setPersonIsParent(!personIsParent)} />
+                      <label className="block text-sm font-medium">Yes</label>
+                    </div>
+                  </div>
+
+
                   <Button className="btn w-full sm:w-1/2 chestnut h-14" onClick={() => {
-                    addFamilyMember(activeFamily, memberAddress, memberName).then(response => {
+                    addFamilyMember(activeFamily, memberAddress, memberName, personIsParent).then(response => {
                       if (response == true) {
                         setMemberName('')
                         setAddressName('')
@@ -109,7 +123,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="">
+      <section className="mb-40">
         <Tabs defaultValue="proposal" className="w-full space-y-6">
           <TabsList className="w-full">
             <div className="container">
@@ -121,7 +135,7 @@ export default function Home() {
           <TabsContent value="family">
             <Families
               listOfFamilies={listOfFamilies}
-              familyMembers={familyMembers}
+              account={account}
             />
           </TabsContent>
 
--- src/components/tabs/contents/families/index.tsx
+++ src/components/tabs/contents/families/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/button";
 import {
   Dialog,
@@ -16,15 +16,22 @@ import {
   AccordionTrigger,
 } from "@/components/accordion"
 import useContractWrite from "@/hooks/write-hooks/useContractWrite";
-import { FamilyMemberType, FamilyType } from "@/types";
+import { FamilyType } from "@/types";
+import { shortenAddress } from "@/functions/format";
+import { copyToClipboard } from "@/functions/misc-functions";
+import { toast } from "@/components/toaster/use-toast";
 
 
-const Families = ({ listOfFamilies, familyMembers }: { listOfFamilies: FamilyType[], familyMembers: FamilyMemberType[] }) => {
-  const { createFamilyAccount } = useContractWrite();
+const Families = (
+  { listOfFamilies, account }:
+  { listOfFamilies: FamilyType[], account: string | undefined }
+) => {
+  const { createFamilyAccount, deleteFamilyAccount } = useContractWrite();
   const [openNewFamily, setOpenNewFamily] = useState<boolean>(false)
+  const [openDeleteFamily, setOpenDeleteFamily] = useState<boolean>(false)
+  const [openRemoveMember, setOpenRemoveMember] = useState<boolean>(false)
   const [familyName, setFamilyName] = useState<string>('')
   const [personName, setPersonName] = useState<string>('')
-
 
   return (
     <>
@@ -43,18 +50,18 @@ const Families = ({ listOfFamilies, familyMembers }: { listOfFamilies: FamilyTyp
               </DialogDescription>
             </DialogHeader>
 
-            <section className="space-y-6 mt-6 w-full pb-12">
+            <section className="space-y-6 mt-6">
               <div className="space-y-2">
-                <label htmlFor="familyName" className="block text-sm/6 font-medium">Family Name</label>
+                <label htmlFor="familyName" className="block text-sm font-medium">Family Name</label>
                 <input id="familyName" type="text" autoComplete="family-name" required className="text-box" value={familyName} onChange={(e) => setFamilyName(e.target.value)} />
               </div>
 
               <div className="space-y-2">
-                <label htmlFor="personalName" className="block text-sm/6 font-medium">Your Name</label>
+                <label htmlFor="personalName" className="block text-sm font-medium">Your Name</label>
                 <input id="personalName" type="text" autoComplete="given-name" required className="text-box" value={personName} onChange={(e) => setPersonName(e.target.value)} />
               </div>
 
-              <Button className="btn w-full sm:w-1/2 chestnut h-14" onClick={() => {
+              <Button className="btn w-full sm:w-1/2 spray-dark h-14" onClick={() => {
                 createFamilyAccount(familyName, personName).then(response => {
                   if (response == true) {
                     setFamilyName('')
@@ -69,23 +76,147 @@ const Families = ({ listOfFamilies, familyMembers }: { listOfFamilies: FamilyTyp
         </Dialog>
       </section>
 
-      <section className="w-full max-w-3xl">
-        {listOfFamilies?.map((family, index) => (
-          <Accordion type="single" collapsible key={index}>
-            <AccordionItem value="item-1">
-              <AccordionTrigger className="bg-white rounded-md p-5 text-lg">{family['name']}</AccordionTrigger>
-
-              <AccordionContent className="bg-gray-50 p-6 rounded-b-md">
-                {familyMembers?.map((member, index) => (
-                  <div className="" key={index}>
-                    {member.name}
+      <section className="w-full max-w-3xl space-y-4">
+        {listOfFamilies.length > 0 ?
+          listOfFamilies?.map((family, index) => (
+            <Accordion type="single" collapsible key={index}>
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="bg-white rounded-md p-5 text-lg">
+                  <div className="text-left">
+                    <h6 className="-mb-3">{family['name']}</h6>
+                    <span className="text-xs">Created by {family.creator === account ? "You" : family.creatorName}</span>
                   </div>
-                ))
-                }
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        ))
+                </AccordionTrigger>
+
+                <AccordionContent className="bg-gray-50 py-6 px-4 rounded-b-md">
+                  <Accordion type="single" collapsible>
+                    <AccordionItem value="item-2">
+                      <AccordionTrigger className="bg-white rounded-md p-5">Family Members</AccordionTrigger>
+
+                      <AccordionContent className="bg-gray-100 py-4 px-6 rounded-b-md">
+                        <div className="flex flex-col">
+                          <div className=" overflow-x-auto">
+                            <div className="min-w-full inline-block align-middle">
+                              <div className="overflow-hidden ">
+                                <table className=" min-w-full rounded-xl">
+                                  <thead>
+                                    <tr className="bg-gray-200 text-left text-sm font-semibold">
+                                      <th scope="col" className="p-3"> Name </th>
+                                      <th scope="col" className="p-3"> Wallet </th>
+                                      <th scope="col" className="p-3"> Role </th>
+                                      <th scope="col" className="p-3"> Actions </th>
+                                    </tr>
+                                  </thead>
+
+                                  <tbody className="divide-y divide-gray-300">
+                                    {family.members?.map((member, index) => (
+                                      <tr className="text-sm font-medium" key={index}>
+                                        <td className="p-3 whitespace-nowrap">{member.name}</td>
+                                        <td className="p-3 whitespace-nowrap">
+                                          <span className="border-dotted border-b border-black cursor-copy" onClick={() => {
+                                            copyToClipboard(member.addr).then(() => toast({ variant: "success", description: "Address successfully copied!" }))
+                                          }}>{shortenAddress(member.addr)}</span>
+                                        </td>
+                                        <td className="p-3 whitespace-nowrap">{Number(member.role) === 1 ? 'Parent' : 'Child'}</td>
+
+                                        <td className=" p-3">
+                                          <Dialog open={openRemoveMember} onOpenChange={setOpenRemoveMember}>
+                                            <DialogTrigger className="text-chestnut-600 text-lg">
+                                              <i className="bi bi-trash3"></i>
+                                            </DialogTrigger>
+
+                                            <DialogContent className="max-w-md w-full bg-white border-0">
+                                              <DialogHeader>
+                                                <DialogTitle>Confirm Remove Family Member</DialogTitle>
+
+                                                <DialogDescription>
+                                                  This action will remove the user from this family.
+                                                </DialogDescription>
+                                              </DialogHeader>
+
+                                              <section className="space-y-8">
+                                                <div className="block text-sm font-medium">
+                                                  You can close this prompt if you don&apos;t wish to continue or click the button below to remove the user.
+                                                </div>
+
+                                                <Button className="btn w-full sm:w-1/2 chestnut h-14" onClick={() => {
+                                                  deleteFamilyAccount(index).then(response => {
+                                                    if (response == true) {
+                                                      setOpenRemoveMember(false);
+                                                    }
+                                                  })
+                                                }}>Yes, Remove Member</Button>
+                                              </section>
+                                            </DialogContent>
+                                          </Dialog>
+                                        </td>
+                                      </tr>
+                                    ))}
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+
+                  <section className="space-y-5 px-4 mt-6 mb-12">
+                    <div className="-space-y-1 text-xs">
+                      <label className="text-base font-semibold">500 AIA</label>
+                      <label className="block">Requires atleast 51% vote or parent&apos;s approval to withdraw</label>
+                    </div>
+
+                    <div className="-space-y-1 text-xs">
+                      <label className="text-base font-semibold">1500 AIA</label>
+                      <label className="block">Requires atleast 75% vote or parent&apos;s approval to withdraw</label>
+                    </div>
+
+                    <div className="-space-y-1 text-xs">
+                      <label className="text-base font-semibold">above 1500 AIA</label>
+                      <label className="block">Requires 100% vote or parent&apos;s approval to withdraw</label>
+                    </div>
+                  </section>
+                  
+                  <Dialog open={openDeleteFamily} onOpenChange={setOpenDeleteFamily}>
+                    <DialogTrigger className="btn px-6 chestnut text-sm">
+                      Delete Family Account
+                    </DialogTrigger>
+
+                    <DialogContent className="max-w-md w-full bg-white border-0">
+                      <DialogHeader>
+                        <DialogTitle>Confirm Delete Family Account</DialogTitle>
+
+                        <DialogDescription>
+                          This action will delete the family account and transfer all the remaining funds in the account to every member of the family.
+                        </DialogDescription>
+                      </DialogHeader>
+
+                      <section className="space-y-8">
+                        <div className="block text-sm font-medium">
+                          You can close this prompt if you don&apos;t wish to continue or click the button below to delete the account.
+                        </div>
+
+                        <Button className="btn w-full sm:w-1/2 chestnut h-14" onClick={() => {
+                          deleteFamilyAccount(index).then(response => {
+                            if (response == true) {
+                              setOpenDeleteFamily(false);
+                            }
+                          })
+                        }}>Yes, Delete Family</Button>
+                      </section>
+                    </DialogContent>
+                  </Dialog>
+
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))
+        :
+          <div className="font-medium text-lg">
+            No family account created yet
+          </div>
         }
       </section>
     </>
