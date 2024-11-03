0226b9972d223c1efbccd3b4cfc10d9ae935e66f
jorniks <dictforum@gmail.com>
2024-11-03 13:20:58 +0100

    Update: removed unused variables

--- src/hooks/read-hooks/useContractRead.ts
+++ src/hooks/read-hooks/useContractRead.ts
@@ -1,19 +1,8 @@
-import { useWeb3React } from "@web3-react/core";
 import { useCallback } from "react";
 import { useContract } from "../services/useContract";
-import { useSetRecoilState } from "recoil";
-import { loadingState } from "@/app/state/atoms/atom";
-import { toast } from "@/components/toaster/use-toast";
-import { errorCode } from "@/lib/metamask-error-codes";
-import { CHAIN_INFO } from "@/lib/services/chain-config";
-import { isAddress } from "ethers";
-import { formatToBigInt } from "@/functions/format";
 
 export default function useContractRead() {
-  const { account, chainId } = useWeb3React();
   const contract = useContract();
-  const setLoadingState = useSetRecoilState(loadingState);
-  const explorerURL = chainId && CHAIN_INFO[chainId].explorer
   
   const getUserFamilies = useCallback(
     async () => {
