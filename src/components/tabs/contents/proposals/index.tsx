f96ee9313662a9fb46fde59078d67b0c0f6c1131
jorniks <dictforum@gmail.com>
2024-11-03 13:14:41 +0100

    Update: created proposal card for more dynamic interaction with proposals

--- src/components/tabs/contents/proposals/index.tsx
+++ src/components/tabs/contents/proposals/index.tsx
@@ -9,17 +9,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/dialog"
-import { formatBalance, formatNumberScale } from "@/functions/format";
 import useContractWrite from "@/hooks/write-hooks/useContractWrite";
-import { FamilyProposals } from "@/types";
-import { useWeb3React } from "@web3-react/core";
+import { FamilyMember, FamilyProposals } from "@/types";
 import { useState } from "react";
+import ProposalCard from "./proposal-card";
 
-const Proposals = ({ familyProposals, activeFamily }: { familyProposals: FamilyProposals[], activeFamily: number }) => {
-  const { account } = useWeb3React()
-  const { createProposal, castVote, claimFunds, vetoProposal } = useContractWrite();
+const Proposals = ({ familyProposals, familyMembers }: { familyProposals: FamilyProposals[], familyMembers: FamilyMember[] }) => {
+  const { createProposal } = useContractWrite();
   const [openNewProposal, setOpenNewProposal] = useState<boolean>(false)
-  const [openMenu, setOpenMenu] = useState<number | null>(null)
   const [proposalObj, setProposalObj] = useState({
     recipient: "",
     description: "",
@@ -107,168 +104,18 @@ const Proposals = ({ familyProposals, activeFamily }: { familyProposals: FamilyP
       </section>
 
       <section className="grid grid-cols-1 md:grid-cols-10 gap-4">
-        {familyProposals?.map((proposal, index) => (
-          <dl key={index} className="md:col-span-2 border hover:shadow-sm duration-500 bg-white rounded flex flex-col">
-            <dt className="p-4 flex items-center justify-between relative">
-              <span className="rounded-full bg-amber-100 px-4 py-1.5 text-sm font-medium text-amber-700">{proposal.proposerName}</span>
-              <i className="bi bi-three-dots-vertical cursor-pointer" onClick={() => setOpenMenu(openMenu ? null : 1)}></i>
-
-              <div className={`absolute right-4 bg-white border w-2/3 rounded shadow top-12 transition-all duration-150 z-10 ${openMenu ? 'scale-y-100 translate-y-0' : 'scale-y-0 -translate-y-1/2'}`}>
-                <div className="border-b p-4 space-y-3">
-                  <span className="font-bold text-xs text-gray-700">For Parents Only</span>
-
-                  <div className="flex items-center justify-between flex-wrap py-2">
-                    <Button className="btn py-2 px-3 spray"
-                      onClick={() => vetoProposal(activeFamily, index, 'approved').then(() => setOpenMenu(null))}
-                    >Approve</Button>
-
-                    <Button className="btn py-2 px-3 chestnut"
-                      onClick={() => vetoProposal(activeFamily, index, 'declined').then(() => setOpenMenu(null))}
-                    >Decline</Button>
-                  </div>
-                </div>
-
-                <div className="p-4">
-                  <span className="font-bold text-xs text-gray-700">Proposal Initiator Only</span>
-
-                  <Button className="btn spray-dark w-full py-2.5"
-                    onClick={() => claimFunds(activeFamily, index).then(() => setOpenMenu(null))}
-                  >Withdraw fund</Button>
-                </div>
-              </div>
-            </dt>
-
-            <div className="p-4 flex-1">
-              <dt className="font-semibold text-wrap">{proposal.title}</dt>
-
-              <dd className="text-sm"> {proposal.description} </dd>
-
-              <div className="">
-                <span className="font-semibold">{ formatNumberScale(formatBalance(proposal.amount)) } AIA</span>
-              </div>
-            </div>
-
-            {/* PROGRESS BAR */}
-            <div className={`flex justify-between text-[0.4rem] font-semibold  h-2 relative`}>
-              <div style={{ width: `${proposal.forPercentage}%` }} className={`spray rounded-l-full`}>
-                <span className="px-1 absolute left-1"> {proposal.forPercentage}% </span>
-              </div>
-
-              <div style={{ width: `${proposal.againstPercentage}%` }} className={`chestnut rounded-r-full`}>
-                <span className="px-1 absolute right-1"> {proposal.againstPercentage}% </span>
-              </div>
-            </div>
-
-            <div className="p-4 items-center grid grid-cols-12">
-              <div className="col-span-7 flex -space-x-2 overflow-hidden">
-                {proposal?.allowedVoters?.slice(0, 4).map((voterAddress, index) => (
-                  <div key={index} className="flex items-center text-sm justify-center font-semibold h-7 w-7 spray-dark rounded-full ring-2 ring-white">{String(voterAddress).substring(index, 2)}</div>
-                ))}
-
-                {proposal?.allowedVoters?.length > 4 &&
-                  <div className="flex items-center justify-center font-medium text-xs h-7 w-7 spray rounded-full ring-2 ring-green-600">+{proposal?.allowedVoters?.length - 4}</div>
-                }
-              </div>
-
-              <div className="col-span-5 flex items-center justify-between">
-                <span className="text-xl flex items-center cursor-pointer hover:text-spray-500 gap-1" onClick={() => castVote(activeFamily, index, true)}>
-                  <i className="bi bi-hand-thumbs-up-fill" title="Vote Yes"></i>
-                  <span className="text-sm">Yes</span>
-                </span>
-
-                <span className="text-xl flex items-center cursor-pointer hover:text-chestnut-500 gap-1" onClick={() => castVote(activeFamily, index, false)}>
-                  <i className="bi bi-hand-thumbs-down-fill" title="Vote No"></i>
-                  <span className="text-sm">No</span>
-                </span>
-              </div>
-            </div>
-          </dl>
-        ))}
-
-        <dl className="md:col-span-2 border hover:shadow-sm duration-500 bg-white rounded flex flex-col">
-          <dt className="p-4 pb-1 flex items-center justify-between relative">
-            <span className="rounded-full bg-amber-100 px-4 py-1.5 text-sm font-medium text-amber-700">Miracle</span>
-            <i className="bi bi-three-dots-vertical cursor-pointer" onClick={() => setOpenMenu(openMenu ? null : 1)}></i>
-
-            <div className={`absolute right-4 bg-white border w-2/3 rounded shadow top-12 transition-all duration-150 z-10 ${openMenu ? 'scale-y-100 translate-y-0' : 'scale-y-0 -translate-y-1/2'}`}>
-              <div className="border-b p-4 space-y-3">
-                <span className="font-bold text-xs text-gray-700">For Parents Only</span>
-
-                <div className="flex items-center justify-between flex-wrap py-2">
-                  <Button className="btn py-2 px-3 spray">Approve</Button>
-                  <Button className="btn py-2 px-3 chestnut">Decline</Button>
-                </div>
-              </div>
-
-              <div className="p-4">
-                <span className="font-bold text-xs text-gray-700">Proposal Initiator Only</span>
-
-                <Button className="btn spray-dark w-full py-2.5"
-                >Withdraw fund</Button>
-              </div>
-            </div>
-          </dt>
-
-          <div className="p-4 flex-1 space-y-2">
-            <dt className="font-semibold text-wrap">Title man of the day</dt>
-
-            <dd className="text-xs">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste sit rerum obcaecati. Dicta rerum, neque voluptate non at, commodi laborum quia labore voluptatem asperiores a natus? Quod fuga fugiat perspiciatis?
-            </dd>
-
-            <div className="mt-4">
-              <span className="font-semibold">20 AIA</span>
-            </div>
-          </div>
-
-          <div className={`flex justify-between text-[0.4rem] font-semibold  h-2 relative mx-2`}>
-            <div style={{ width: `77%` }} className={`spray rounded-l-full`}>
-              <span className="px-1 absolute left-1"> 50% </span>
-            </div>
-
-            <div style={{ width: `23%` }} className={`chestnut rounded-r-full`}>
-              <span className="px-1 absolute right-1"> 50% </span>
-            </div>
-          </div>
-
-          <div className="p-4 items-center grid grid-cols-12">
-            <div className="col-span-7 flex -space-x-2 overflow-hidden">
-              <div className="flex items-center text-sm justify-center font-semibold h-7 w-7 spray-dark rounded-full ring-2 ring-white">{String('0x32fe').substring(0, 2)}</div>
-              <div className="flex items-center text-sm justify-center font-semibold h-7 w-7 spray-dark rounded-full ring-2 ring-white">{String('0x32fe').substring(0, 2)}</div>
-              <div className="flex items-center text-sm justify-center font-semibold h-7 w-7 spray-dark rounded-full ring-2 ring-white">{String('0x32fe').substring(0, 2)}</div>
-              <div className="flex items-center text-sm justify-center font-semibold h-7 w-7 spray-dark rounded-full ring-2 ring-white">{String('0x32fe').substring(0, 2)}</div>
-            </div>
-
-            <div className="col-span-5 flex items-center justify-between">
-              <span className="text-xl flex items-center cursor-pointer hover:text-spray-500 gap-1">
-                <i className="bi bi-hand-thumbs-up-fill" title="Vote Yes"></i>
-                <span className="text-sm">Yes</span>
-              </span>
-
-              <span className="text-xl flex items-center cursor-pointer hover:text-chestnut-500 gap-1">
-                <i className="bi bi-hand-thumbs-down-fill" title="Vote No"></i>
-                <span className="text-sm">No</span>
-              </span>
-            </div>
-          </div>
-        </dl>
-
-        <dl className="md:col-span-2 divide-y border hover:shadow-sm duration-500 bg-white rounded flex flex-col">
-          <div className="p-4 flex-1">
-            <dt className="mb-6 flex items-center justify-between">
-              <span className="rounded-full bg-amber-100 px-4 py-1.5 text-sm font-medium text-amber-700">Miracle</span>
-              <i className="bi bi-three-dots-vertical"></i>
-            </dt>
-
-            <dd className="text-sm mt-3">
-              Dicta rerum, neque voluptate non at, commodi laborum quia labore voluptatem asperiores a natus? Quod fuga fugiat perspiciatis?
-            </dd>
-          </div>
-
-          <div className="p-4">
-            frerger
-          </div>
-        </dl>
+        {familyProposals?.length > 0 ?
+          familyProposals?.map((proposal, index) => (
+            <ProposalCard
+              key={index}
+              proposalIndex={index}
+              proposal={proposal}
+              familyMembers={familyMembers}
+            />
+          ))
+        :
+          <div className="font-medium text-lg col-span-12">No proposals created in this family yet</div>
+        }
       </section>
     </>
   )
---
+++ src/components/tabs/contents/proposals/proposal-card.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { Button } from "@/components/button";
+import { FamilyMember, FamilyProposals } from "@/types";
+import { useState } from "react";
+import useContractWrite from "@/hooks/write-hooks/useContractWrite";
+import { useRecoilValue } from "recoil";
+import { activeFamilyIndexState } from "@/app/state/atoms/atom";
+import { formatBalance, formatNumberScale } from "@/functions/format";
+
+const ProposalCard = ({ proposal, proposalIndex, familyMembers }: { proposal: FamilyProposals, proposalIndex: number, familyMembers: FamilyMember[] }) => {
+  const { castVote, claimFunds, vetoProposal } = useContractWrite();
+  const [openMenu, setOpenMenu] = useState<boolean>(false)
+  const familyId = useRecoilValue(activeFamilyIndexState)
+
+  return (
+    <dl className="md:col-span-2 border hover:shadow-sm duration-500 bg-white rounded flex flex-col">
+      <dt className="p-4 pb-1 flex items-center justify-between relative">
+        <span className="rounded-full px-4 py-1.5 text-sm font-medium bg-amber-100 text-amber-700">{proposal?.status}</span>
+        <i className="bi bi-three-dots-vertical cursor-pointer" onClick={() => setOpenMenu(!openMenu)}></i>
+
+        <div className={`absolute right-4 bg-white border w-2/3 rounded shadow top-12 transition-all duration-150 z-10 ${openMenu ? 'scale-y-100 translate-y-0' : 'scale-y-0 -translate-y-1/2'}`}>
+          <div className="border-b p-4 space-y-3">
+            <span className="font-bold text-xs text-gray-700">For Parents Only</span>
+
+            <div className="flex items-center justify-between flex-wrap py-2">
+              <Button className="btn py-2 px-3 spray"
+                onClick={() => vetoProposal(familyId, proposalIndex, 'approved').then(() => setOpenMenu(false))}
+              >Approve</Button>
+
+              <Button className="btn py-2 px-3 chestnut"
+                onClick={() => vetoProposal(familyId, proposalIndex, 'declined').then(() => setOpenMenu(false))}
+              >Decline</Button>
+            </div>
+          </div>
+
+          <div className="p-4">
+            <span className="font-bold text-xs text-gray-700">Proposal Initiator Only</span>
+
+            <Button className="btn spray-dark w-full py-2.5"
+              onClick={() => claimFunds(familyId, proposalIndex).then(() => setOpenMenu(false))}
+            >Withdraw fund</Button>
+          </div>
+        </div>
+      </dt>
+
+      <div className="p-4 flex-1 space-y-2">
+        <dt className="font-semibold text-wrap">{proposal.title}</dt>
+        <dd className="text-xs"> {proposal.description} </dd>
+      </div>
+
+      <div className="px-4 py-2 font-semibold"> {formatNumberScale(formatBalance(proposal.amount))} AIA </div>
+
+      <div className="p-4 items-center grid grid-cols-12 border-t-2">
+        <div className="col-span-7 flex -space-x-2.5 overflow-hidden">
+          {familyMembers?.slice(0, 4).map((member, index) => (
+            <div key={index} className="flex items-center justify-center h-7 w-7 rounded-full ring-2 spray-dark ring-white font-semibold" title={member.name}>
+              <i className="bi bi-person-fill"></i>
+            </div>
+          ))}
+
+          {familyMembers?.length > 4 &&
+            <div className="flex items-center justify-center h-7 w-7 rounded-full ring-2 spray-dark ring-white font-medium text-xs">+{familyMembers?.length - 4}</div>
+          }
+        </div>
+
+        <div className="col-span-5 flex items-center justify-between">
+          <Button className="text-xl flex items-center text-green-600 gap-1" onClick={() => castVote(familyId, proposalIndex, true)}>
+            <i className="bi bi-hand-thumbs-up-fill" title="Vote Yes"></i>
+            <span className="text-sm">Yes</span>
+          </Button>
+
+          <Button className="text-xl flex items-center text-chestnut-600 gap-1" onClick={() => castVote(familyId, proposalIndex, false)}>
+            <i className="bi bi-hand-thumbs-down-fill" title="Vote No"></i>
+            <span className="text-sm">No</span>
+          </Button>
+        </div>
+      </div>
+    </dl>
+  )
+}
+
+export default ProposalCard
